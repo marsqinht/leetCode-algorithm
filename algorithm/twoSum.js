eee152c8480561643955d33cbbb969bd9a5dd471
秦海涛 <qinhaitao@duiba.com.cn>
2018-06-14 16:11:12 +0800

    two sum

---
+++ algorithm/twoSum.js
@@ -0,0 +1,54 @@
+/**
+ * title: 两数之和
+ * 给定一个整数数组和一个目标值， 找出数组中和为目标值的两个数。
+ * 你可以假设每个输入只对应一种答案， 且同样的元素不能被重复利用。
+ * 
+ * 示例: 
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回[0, 1]
+ */
+
+ const nums = [2, 7, 11, 15];
+
+ const twoSum = function (nums, target) {
+   for (let i = 0, len = nums.length; i < len; i++) {
+     const num1 = nums[i]  
+     for (let j = i + 1, len = nums.length; j < len; j++) {
+        num2 = nums[j]
+        if(num1 + num2 === target) {
+          return [i, j]
+        }
+     }
+   }
+   return []
+ };
+
+ const twoSumII = function (nums, target) {
+   let map = new Map();
+   for (let i = 0, len = nums.length; i < len; i++) {
+     map.set(nums[i], i);
+   }
+   for (let i = 1, len = nums.length; i < len; i++) {
+     let el = target - nums[i];
+     if(map.has(el)) {
+       return [map.get(el), i].sort()
+     }
+   }
+   return []
+ }
+
+  const twoSumIII = function (nums, target) {
+    const map = {};
+    for (let i = 0, len = nums.length; i < len; i++) {
+      let el = target - nums[i];
+      if (map[el] !== undefined) {
+        return [map[el], i]
+      }
+      map[nums[i]] = i;
+    }
+    return []
+  };
+
+
+ console.log(twoSumII(nums, 18));
